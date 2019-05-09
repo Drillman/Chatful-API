69acafaaf92c14efd2639ce7f2749cbd1bd365c3
Lucas <xz351@hotmail.fr>
2019-05-09 11:09:38 +0200

    Fix index

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
-<?php 
+<?php
 $test = $_POST['firstQuestion'];
 
-$url = './data.json';
-$data = file_get_content($url);
+// $url = './data.json';
+// $data = file_get_content($url);
 
 $data = '{
     "messages": [
         {"text": "Welcome to the Chatfuel!"},
         {"text": "'.$test.'"}
     ]
-};
+}';
 
 $data = json_decode($data);
 $data = json_encode($data);
 header('Acces-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 echo $data;
-
