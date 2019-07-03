329dc4eceb8ee7ea1a67c56fc8d16b3923a17617
Lucas <xz351@hotmail.fr>
2019-07-03 18:57:39 +0200

    Errors tests

--- src/routers/weather.routes.ts
+++ src/routers/weather.routes.ts
@@ -11,8 +11,12 @@ router.post('/weather', function(req: express.Request, res: express.Response) {
   request
     .get(`${config.WEATHER_API_URL}/weather?APPID=${config.WEATHER_API_KEY}&q=${city}&units=metric`)
     .then(result =>{
-      console.log(result)
-      result = JSON.parse(result)
+      try {
+        result = JSON.parse(result)
+      } catch (err) {
+        console.error(result)
+        console.error(err)
+      }
       const response = ChatfulHelper.createMessage(`Currently in ${city} there's ${result.weather[0].description} and the temperature is ${result.main.temp} degres celcius`)
       res.json(response)
     })
