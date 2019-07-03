c3d52eb9cddbfb0acf035d2671f1c1e995b421d4
Lucas <xz351@hotmail.fr>
2019-07-03 18:26:35 +0200

    Working routes

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 config =  {
   DB_URL: process.env.DB_URL || 'ds153556.mlab.com:53556/chatful',
   DB_USERNAME: process.env.DB_USERNAME || 'apiuser',
-  DB_PASSWORD: process.env.DB_PASSWORD || 'apiuser1'
+  DB_PASSWORD: process.env.DB_PASSWORD || 'apiuser1',
+  WEATHER_API_KEY: 'aaef115d2738ce542bb9078dcfba9474',
+  WEATHER_API_URL: 'https://api.openweathermap.org/data/2.5'
 }
 
 module.exports = config
--- src/routers/weather.routes.ts
+++ src/routers/weather.routes.ts
@@ -1,12 +1,27 @@
 import * as express from 'express'
 import {Router} from 'express'
+import * as request from "request-promise";
+import * as config from '../../config'
 import ChatfulHelper from '../helpers/chatful.helper';
 
 const router = Router()
 
-router.get('/weather', function(req: express.Request, res: express.Response) {
-  const result = ChatfulHelper.createMessage(`I'm not the weather man for god sake !`)
-  res.send(result);
+router.post('/weather', function(req: express.Request, res: express.Response) {
+  const city = req.body.city
+  request
+    .get(`${config.WEATHER_API_URL}/weather?APPID=${config.WEATHER_API_KEY}&q=${city}&units=metric`)
+    .then(result => {
+      result = JSON.parse(result)
+      const response = ChatfulHelper.createMessage(`Currently in ${city} there's ${result.weather[0].description} and the temperature is ${result.main.temp} degres celcius`)
+      res.json(response)
+    })
+    .catch(error => {
+      if (error.statusCode == 404) {
+        const response = ChatfulHelper.createMessage(`It seems that ${city} does not exist`)
+        return res.json(response)
+      }
+      res.status(500).send(error)
+    })
 })
 
 
